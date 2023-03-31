51a775e59d6d3d04cf9ef4d6ad5e185a25b2cb87
Derek Wheeler <frozenarrow73@gmail.com>
2023-03-31 10:54:52 -0600

    completed step one

--- runtime.js
+++ runtime.js
@@ -56,7 +56,71 @@ perf.start();
 doublerInsert(extraLargeArray);
 let resultsInsert = perf.stop();
 
+//---------------------------------------------------------------------------------------------
+perf.start();                    
+doublerAppend(tinyArray);
+let tinyArrayAppend = perf.stop(); 
 
+perf.start();
+doublerInsert(tinyArray);
+let tinyArrayInsert = perf.stop();
+
+//--------------------------------
+
+perf.start();                    
+doublerAppend(smallArray);
+let smallArrayAppend = perf.stop(); 
+
+perf.start();
+doublerInsert(smallArray);
+let smallArrayInsert = perf.stop();
+
+//--------------------------------
+
+perf.start();                    
+doublerAppend(mediumArray);
+let mediumArrayAppend = perf.stop(); 
+
+perf.start();
+doublerInsert(mediumArray);
+let mediumArrayInsert = perf.stop();
+
+//--------------------------------
+
+perf.start();                    
+doublerAppend(largeArray);
+let largeArrayAppend = perf.stop(); 
+
+perf.start();
+doublerInsert(largeArray);
+let largeArrayInsert = perf.stop();
+
+//---------------------------------------------
+console.log('Results for the tinyArray');
+console.log("insert", tinyArrayInsert.preciseWords);
+console.log("append", tinyArrayAppend.preciseWords);
+
+console.log("------------------------------------")
+
+console.log('Results for the smallArray');
+console.log("insert", smallArrayInsert.preciseWords);
+console.log("append", smallArrayAppend.preciseWords);
+
+console.log("------------------------------------")
+
+console.log('Results for the mediumArray');
+console.log("insert", mediumArrayInsert.preciseWords);
+console.log("append", mediumArrayAppend.preciseWords);
+
+console.log("------------------------------------")
+
+console.log('Results for the largeArray');
+console.log("insert", largeArrayInsert.preciseWords);
+console.log("append", largeArrayAppend.preciseWords);
+
+console.log("------------------------------------")
+
+//---------------------------------------------
 console.log('Results for the extraLargeArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
